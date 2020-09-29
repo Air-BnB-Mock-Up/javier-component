561aa8c81969df65a71002e4cd8d4b2f7b14d0aa
Javier Flores <javier.floresapodaca@gmail.com>
2020-09-28 19:00:49 -0700

    Webpack setup complete

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Reviews</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname + '/client/src/index.jsx'),
+  output: {
+    path: path.join(__dirname + '/client/public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+
+        }
+      }
+    ]
+
+  }
+}
